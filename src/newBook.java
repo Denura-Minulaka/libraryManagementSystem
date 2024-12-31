dfd15728cd6ca48d6d6b58e99069bf6402e7c48d
Denura-Minulaka <denura.minulaka@gmail.com>
2024-12-31 21:55:34 +0530

    newBook interface
    increased the function in newBook interface

--- src/newBook.java
+++ src/newBook.java
@@ -41,6 +41,8 @@ public class newBook extends javax.swing.JFrame {
         jTextField5 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -118,6 +120,26 @@ public class newBook extends javax.swing.JFrame {
         });
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(361, 342, -1, -1));
 
+        jButton3.setBackground(new java.awt.Color(255, 255, 153));
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton3.setText("Update");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 400, -1, -1));
+
+        jButton4.setBackground(new java.awt.Color(255, 255, 153));
+        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jButton4.setText("Delete");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 400, -1, -1));
+
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/123456.png"))); // NOI18N
         jLabel6.setText("jLabel6");
         getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, -1));
@@ -154,6 +176,56 @@ public class newBook extends javax.swing.JFrame {
             new newBook().setVisible(true);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        String bookID = jTextField1.getText();
+        String name = jTextField2.getText();
+        String publisher = JTextField3.getText();
+        String price = jTextField4.getText();
+        String publisherYear = jTextField5.getText();
+        
+        
+        try {
+            Connection conn = singletonConnection.getCon();
+            PreparedStatement ps = conn.prepareStatement("update book set name=?,publisher=?,price=?,publisherYear=? where bookID=?");
+            ps.setString(1, name);
+            ps.setString(2, publisher);
+            ps.setString(3, price);
+            ps.setString(4, publisherYear);
+            ps.setString(5, bookID);
+            ps.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "successfuly updated");
+            setVisible(false);
+            new newBook().setVisible(true);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+            setVisible(false);
+            new newBook().setVisible(true);
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        String bookID = jTextField1.getText();
+        
+        
+        
+        try {
+            Connection conn = singletonConnection.getCon();
+            PreparedStatement ps = conn.prepareStatement(" Delete from book where bookID=?");
+            
+            ps.setString(1, bookID);
+            ps.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "successfuly Deleted");
+            setVisible(false);
+            new newBook().setVisible(true);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,e.getMessage());
+            setVisible(false);
+            new newBook().setVisible(true);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -194,6 +266,8 @@ public class newBook extends javax.swing.JFrame {
     private javax.swing.JTextField JTextField3;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
